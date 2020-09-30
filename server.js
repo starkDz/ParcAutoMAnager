f00851967b5439e94c9eac209ad4489a02816b8e
Djehinet Fateh <fateh_djehinet@outlook.com>
2020-09-30 18:16:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+connectDB();
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Serve started on port ' + PORT));
